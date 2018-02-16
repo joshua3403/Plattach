ab24cd87ff1fc784a8b5904d5cd4cb42fce526c5
JOSHUA-3403\joshua <joshua3403@gmail.com>
2018-02-16 15:35:35 -0400

    Plattach

---
+++ Assets/TitleScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel("GameScene");
+        }
+	}
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 128, 32), "Plattach");
+    }
+}
